a25e02d55f18c6b2200e04508567a14c7eddb04d
Александра Москалева <parsivaasol@gmail.com>
2026-09-18 22:04:11 +0300

    базовый роунтинг для главной и истории

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
-import Home from './pages/Home';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import HistoryPage from "./pages/HistoryPage";
 
-function App() {
-  return <Home />;
+export function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/history" element={<HistoryPage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 }
-
-export default App;
